aa2e31b1226e16620b90efb97b1a166bcae6e733
Chuck Bassett <iamchuckb@gmail.com>
2016-11-23 00:22:22 -0500

    Can haz lexer plz

---
+++ jsonish/lexer.py
@@ -0,0 +1,46 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import argparse
+import re
+import sys
+
+
+
+class TokenMeta(type):
+    def __new__(cls, name, parents, dct):
+        if not dct.get('regex') and not dct.get('regex_str'):
+            raise TypeError('Literal classes must be defined with a regex')
+        elif not dct.get('regex'):
+            regex_str = dct.get('regex_str')
+            dct['regex'] = re.compile(regex_str)
+        return super(TokenMeta, cls).__new__(cls, name, parents, dct)
+
+
+class Token(object):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return '<{} (value: {})>'.format(self.__class__.__name__, self.value)
+
+
+def token_class_factory(name, regex_str):
+    return TokenMeta(name.encode('ascii'), (Token,), {'regex_str': regex_str})
+
+
+STRING_PATTERN = r'^"(\\"|\\\\|\\/|\b|\f|\n|\r|\t|\\u[A-Fa-f0-9]{4}|[^"\\])*"$'
+NUMBER_PATTERN = r'^-?(0|[1-9]\d*)(\.\d+)?([eE][+-]?\d+)?$'
+BOOLEAN_PATTERN = '^(true|false)$'
+
+
+OBJ_START_LITERAL = token_class_factory('OBJ_START_LITERAL', r'^{$')
+OBJ_END_LITERAL = token_class_factory('OBJ_END_LITERAL', r'^}$')
+ARRAY_START_LITERAL = token_class_factory('ARRAY_START_LITERAL', r'^\[$')
+ARRAY_END_LITERAL = token_class_factory('ARRAY_END_LITERAL', r'^\]$')
+COMMA_LITERAL = token_class_factory('COMMA_LITERAL', r'^,$')
+COLON_LITERAL = token_class_factory('COLON_LITERAL', r'^:$')
+NULL_LITERAL = token_class_factory('NULL_LITERAL', r'^null$')
+STRING_LITERAL = token_class_factory('STRING_LITERAL', STRING_PATTERN)
+NUMBER_LITERAL = token_class_factory('NUMBER_LITERAL', NUMBER_PATTERN)
+BOOLEAN_LITERAL = token_class_factory('BOOLEAN_LITERAL', BOOLEAN_PATTERN)
---
+++ tests/test_lexer.py
@@ -0,0 +1,81 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import unittest
+
+from jsonish import lexer
+
+
+class TextRegexes(unittest.TestCase):
+
+    def test_string_literal(self):
+        good_strs = [
+            r'""',
+            r'"a"',
+            r'"\""',
+            r'"\\a\\b\\\""',
+            r'"\"\\\"\\\""',
+            r'"42"',
+        ]
+        bad_strs = [
+            r'',
+            r' ""',
+            r'"" ',
+            r'"\\""',
+            r"''",
+            r'"\\\\""',
+            r'nostartquote"',
+            r'"noendquote',
+            r'42',
+            r'',
+        ]
+        for good_str in good_strs:
+            self.assertIsNotNone(lexer.STRING_LITERAL.regex.match(good_str))
+        for bad_str in bad_strs:
+            self.assertIsNone(lexer.STRING_LITERAL.regex.match(bad_str))
+
+    def test_number_literal(self):
+        good_nums = [
+            r'1',
+            r'0',
+            r'1.0',
+            r'21.234',
+            r'-32.233e4',
+            r'12E+30',
+        ]
+        bad_nums = [
+            r'01',
+            r'1.',
+            r'.23',
+            r'1+e',
+            r'1+e5',
+            r'+1',
+            r'0-1',
+            r'1.0.0',
+            r'12E+30-5',
+            r'"42"',
+            r'',
+        ]
+        for good_num in good_nums:
+            self.assertIsNotNone(lexer.NUMBER_LITERAL.regex.match(good_num))
+        for bad_num in bad_nums:
+            self.assertIsNone(lexer.NUMBER_LITERAL.regex.match(bad_num))
+
+    def test_simple_literals(self):
+        for token, s in (
+                (lexer.OBJ_START_LITERAL, '{'),
+                (lexer.OBJ_END_LITERAL, '}'),
+                (lexer.ARRAY_START_LITERAL, '['),
+                (lexer.ARRAY_END_LITERAL, ']'),
+                (lexer.COMMA_LITERAL, ','),
+                (lexer.COLON_LITERAL, ':'),
+                (lexer.NULL_LITERAL, 'null'),
+                (lexer.BOOLEAN_LITERAL, 'true'),
+                (lexer.BOOLEAN_LITERAL, 'false'),
+        ):
+            self.assertIsNotNone(token.regex.match(s))
+            self.assertIsNone(token.regex.match(s + ' '))
+            self.assertIsNone(token.regex.match(' ' + s))
+            self.assertIsNone(token.regex.match(s + s))
+            if s.upper() != s:
+                self.assertIsNone(token.regex.match(s.upper()))
